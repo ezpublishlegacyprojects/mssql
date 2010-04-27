e48a8687e90e7abe1cc2ce669b42cc773576d1a7
Björn Dieding <bjoern@xrow.de>
2010-04-27 14:23:07 +0000

    created branch for ez 3.x
    git-svn-id: http://svn.projects.ez.no/mssql@8 03075cdf-6d20-4ca6-a575-56f24e3d3604

---
+++ stable/2.0/classes/version.php
@@ -0,0 +1,20 @@
+<?php
+/*
+    eZ Publish MSSQL extension
+    Copyright (C) 2007  xrow GbR, Hannover, Germany
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
+define( "EZ_MSSQL_VERSION_REVISION_STRING", '$Rev: 174 $' );
+define( "EZ_MSSQL_VERSION_REVISION", preg_replace( "#\\\$Rev:\s+([0-9]+)\s+\\\$#", '$1', EZ_MSSQL_VERSION_REVISION_STRING ) );
+
+?>
